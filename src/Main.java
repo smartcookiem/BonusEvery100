55cfc1282a0223a84198098cfec20630536388a2
Olga Cookie <angel_2008_year@mail.ru>
2023-01-24 21:23:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountLeft = 10;
+        int bonusEvery = 100;
+        int replenishment = 700;
+        int bonus = replenishment / bonusEvery;
+
+        if (replenishment < 200) {
+            bonus = 0;
+        }
+        int finalAmount = amountLeft + replenishment + bonus;
+
+        System.out.println("Остаток на счете " + finalAmount);
+    }
+
+}
